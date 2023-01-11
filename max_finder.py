60168422996d0d608f8ebb0a321e2f81981be540
Luis Eduardo Craizer <lecraizer@tecgraf.puc-rio.br>
2023-01-11 12:17:30 -0300

    max finder working on maximizing [0,1] functions

--- max_finder.py
+++ max_finder.py
@@ -9,6 +9,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import sys
 import random
 import numpy as np
+from math import log, sin
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -38,7 +39,6 @@ print('Batch size:', BS)
 
 
 ####### -------- Auxiliar functions -------- ####### 
-
 def create_batch(batch_size=8):
     '''
     Create new data input for the model in which all players are 
@@ -49,36 +49,42 @@ def create_batch(batch_size=8):
 
     X1 = []
     X2 = []
-
+    
     for k in range(batch_size):
         x1 = random.random()
         x2 = random.random()
-        # X.append( (x1, x2) )
         X1.append( x1 )
         X2.append( x2 )
         R = x2 - (x1-x2)**2
+        # R = -(x2-log(x1)+x1**2)
+        # R = -(0.8-sin(x1)+x1**2)
         y.append(R)
 
-    # return np.array(X), np.array(y)
     return np.array(X1), np.array(X2), np.array(y)
 
 
-def create_model(prec=10, loss='mse'):
+def create_model(prec=10, loss='mse', extra_layer=False):
     ''' 
     Initialize and create model
     '''
+    activ_func = 'relu'
     first_input = Input(shape=(1, ))
-    first_dense = Dense(1, use_bias=False)(first_input)
+
+    if extra_layer:
+        first_dense = Dense(1, use_bias=False)(first_input) 
+        first_dense = Dense(1, use_bias=False)(first_dense)
+    else:
+        first_dense = Dense(1, use_bias=False)(first_input)
 
     second_input = Input(shape=(1, ))
     second_dense = Dense(1, use_bias=False)(second_input)
 
     merged = concatenate([first_dense, second_dense])
-    x3 = Dense(20, activation='tanh', use_bias=False)(merged)
-    x3 = Dense(100, activation='tanh', use_bias=False)(x3)
-    x3 = Dense(400, activation='tanh', use_bias=False)(x3)
-    x3 = Dense(100, activation='tanh', use_bias=False)(x3)
-    x3 = Dense(20, activation='tanh', use_bias=False)(x3)
+    x3 = Dense(20, activation=activ_func, use_bias=False)(merged)
+    x3 = Dense(100, activation=activ_func, use_bias=False)(x3)
+    x3 = Dense(400, activation=activ_func, use_bias=False)(x3)
+    x3 = Dense(100, activation=activ_func, use_bias=False)(x3)
+    x3 = Dense(20, activation=activ_func, use_bias=False)(x3)
 
     # Final layer
     result = Dense(1, activation='linear', use_bias=False)(x3)
@@ -87,7 +93,10 @@ def create_model(prec=10, loss='mse'):
     model = Model(inputs=[first_input, second_input], outputs=result)
 
     model.compile(optimizer=opt, loss=loss)
-    plot_model(model, to_file='stuff/model_plot.png', show_shapes=True, show_layer_names=False)
+    if not extra_layer:
+        plot_model(model, to_file='stuff/model_plot.png', show_shapes=True, show_layer_names=False)
+    else:
+        plot_model(model, to_file='stuff/new_model_plot.png', show_shapes=True, show_layer_names=False)
     print(model.summary())
     return model
 
@@ -103,6 +112,31 @@ def train(epochs, batch_size=BS, save_interval=1000):
         
         if epoch % save_interval == 0:
             print ("Epoch: %d | Loss: %f" % (epoch, loss))
+
+
+def retrain(epochs, batch_size=BS, save_interval=1000):
+    ''' Main function, used for training
+    '''
+    for epoch in range(epochs):
+        y = []
+        X1 = []
+        X2 = []
+        for k in range(batch_size):
+            X1.append( 1 )
+            X2.append( 0.8 )
+            # R = 0.8 - (1-0.8)**2
+            R = 10000000
+            y.append(R)
+            
+        X_batch1, X_batch2, y_batch = np.array(X1), np.array(X2), np.array(y)
+        loss = new_model.train_on_batch(x=[X_batch1, X_batch2], y=y_batch)
+        
+        if epoch % save_interval == 0:
+            print ("Epoch: %d | Loss: %f" % (epoch, loss))
+
+
+def custom_loss_function(y_true, y_pred):
+    return 100 - y_pred
 
 
 ####### -------- Initialize hiperparameters -------- ####### 
@@ -124,31 +158,36 @@ train(n_epochs, batch_size=BS)
 
 ####### -------- Create new model with frozen layers -------- ####### 
 
-print('\nFirst model:\n')
-for layer in model.layers:
-    print(layer.name, layer.trainable)
-
-new_model = create_model(loss_func)
+new_model = create_model(custom_loss_function, extra_layer=True)
 weights = model.get_weights()
+weights.insert(0, np.expand_dims(np.ndarray(1), axis=0)) # creating synthetic weight element
 new_model.set_weights(weights)
 
 for layer in new_model.layers:
     if layer.name != 'dense_9':
         layer.trainable = False
 
-print('\nSecond model:\n')
+print('\nNew model:\n')
 for layer in new_model.layers:
     print(layer.name, layer.trainable)
 
 print('\n\n')
-print('Layer name:', new_model.layers[2].name)
-
-w = new_model.layers[2].get_weights()[0][0][0]
+print('Layer name:', new_model.layers[1].name)
+w = new_model.layers[1].get_weights()[0][0][0]
 print('Weights:', w)
 
-### input = [1, 0.8]
-### w treinável
-### L = (100 - ŷ)
+new_model.compile(optimizer=opt, loss=custom_loss_function)
+
+
+####### -------- New training with frozen layers except one -------- ####### 
+
+print('Train new model!\n')
+retrain(n_epochs, batch_size=BS)
+print('\n\n')
+print('Layer name:', new_model.layers[1].name)
+w = new_model.layers[1].get_weights()[0][0][0]
+print('Weights:', w)
+
 
 ####### -------- Test results -------- ####### 
 
